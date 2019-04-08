f86f1fdb30b4df919518461592178e713f3f785c
scentelles <scentelles@gmail.com>
2019-04-08 08:00:02 +0200

    load presets from ROM, and add display of note beeing triggered

--- LaserKeyboard.cpp
+++ LaserKeyboard.cpp
@@ -1,16 +1,17 @@
 #include "LaserKeyboard.h"
 
-LaserKeyboard::LaserKeyboard(Adafruit_MCP23017 * mcp)
+
+
+LaserKeyboard::LaserKeyboard(Adafruit_MCP23017 * mcp, LiquidCrystal_I2C * lcdDisplay)
 {
+
+  lcd_ = lcdDisplay;
+  
   //todo : get from EEPROM
-  //init beam to note map.
-  beamIdToNoteMap[0] = 42;
-  beamIdToNoteMap[1] = 43;
-  beamIdToNoteMap[2] = 44;
-  beamIdToNoteMap[3] = 54;
-  beamIdToNoteMap[4] = 64;
-  beamIdToNoteMap[5] = 74;
-  beamIdToNoteMap[6] = 84;
+  notePresets_.push_back({45, 56, 60, 62, 63, 45, 21});
+  readAllPresetsFromROM();
+  
+  loadPreset(currentPreset_);
 
   beamToPinMap[0] = 8;
   beamToPinMap[1] = 9;
@@ -20,22 +21,113 @@ LaserKeyboard::LaserKeyboard(Adafruit_MCP23017 * mcp)
   beamToPinMap[5] = 13;
   beamToPinMap[6] = 14;  
 
-  myOSCManager_ = new OSCManager();
+ // IPAddress* remoteIPAddress = new IPAddress(192,168,1,51);
+  IPAddress* remoteIPAddress = new IPAddress(192,168,1,64);
+  myOSCManager_ = new OSCManager(remoteIPAddress, 8000, 8888);
   mcp_ = mcp;
 }
 //todo : destructor
+void LaserKeyboard::readAllPresetsFromROM()
+{
+
+  EEPROM.begin(512);
+    int nbPresetFromROM =  (char)EEPROM.read(NB_PRESET_ADDRESS);
+    //TODO : find another way...
+    if(nbPresetFromROM < 1 || nbPresetFromROM > 32)
+        nbPresetFromROM = 1;
+    Serial.print("Found nb presets in ROM :");
+    Serial.println(nbPresetFromROM);
+    //skip first one as wee need at least one preset untouched
+    for(int i = 1; i < nbPresetFromROM;i++)
+    {
+      int addr  =  i * BANK_SIZE;      
+      std::array<int, NB_BEAM> tempPreset;
+      for(int j=0; j < NB_BEAM-1; j++)
+      { 
+        char tempC = (char)EEPROM.read(addr + i);
+        
+        tempPreset[j] = tempC;
+      }
+      notePresets_.push_back(tempPreset);
+      Serial.println("Added new preset from ROM");
+    }
+  
+  }
+  
+void LaserKeyboard::storePreset(int index, std::array<int, NB_BEAM> * thisPreset)
+{
+  int addr  = index * BANK_SIZE;
+  EEPROM.begin(512);
+    for(int i=0; i < NB_BEAM-1; i++)
+    { 
+      EEPROM.write(addr + i, (*thisPreset)[i]);
+      Serial.print("Writing ");
+      Serial.print((*thisPreset)[i]);
+      Serial.print("to address");
+      Serial.println(addr + i);
+    }
+    Serial.print("Storing Preset at index ");
+    Serial.println(index);
+    EEPROM.write(NB_PRESET_ADDRESS, notePresets_.size());
+    EEPROM.commit();  
+}
+
+
+int LaserKeyboard::getCurrentPreset()
+{
+  return currentPreset_;
+}
+
+void LaserKeyboard::addPresetFromCurrent()
+{
+  std::array<int, NB_BEAM> tempPreset = notePresets_[currentPreset_];
+  notePresets_.push_back(tempPreset);
+  storePreset(notePresets_.size()-1, &tempPreset);
+}
+
+void LaserKeyboard::loadPreset(int presetIndex)
+{
+  //exclude out of bound preset
+  if((presetIndex >=  notePresets_.size()) || (presetIndex < 0))
+      return;
+      
+  currentPreset_ = presetIndex;
+  for(int i=0; i<NB_BEAM; i++)
+  {
+      beamIdToNoteMap[i] = notePresets_[presetIndex][i];
+  }
+   Serial.println("=======================");
+    Serial.println(beamIdToNoteMap[0]);
+  Serial.println(beamIdToNoteMap[1]);
+  Serial.println(beamIdToNoteMap[2]);
+  Serial.println(beamIdToNoteMap[3]);
+  Serial.println(beamIdToNoteMap[4]);
+    Serial.println(beamIdToNoteMap[5]);
+      Serial.println(beamIdToNoteMap[6]);
+           Serial.println("=======================");
+}
 
 void LaserKeyboard::setup()
 {
+
   myOSCManager_->setup();
 }
 
-void LaserKeyboard::setBeamIdToNote(int beamId, int note)
+
+
+  
+/*void LaserKeyboard::setBeamIdToNote(int beamId, int note)
 {
 
   //store in local
-  beamIdToNoteMap[beamId] = note;
+ // beamIdToNoteMap[beamId] = note;
   //store in EEPROM
+  notePresets_[currentPreset_]
+}*/
+
+void LaserKeyboard::displayBeamStatus(int beamId)
+{
+  
 }
 
 void LaserKeyboard::process_beam_event(int beamId, bool value)
@@ -45,13 +137,26 @@ void LaserKeyboard::process_beam_event(int beamId, bool value)
         Serial.print(beamId);
         Serial.print(" : ");
         Serial.println(value);
+        Serial.print("Current Preset : ");
+        Serial.println(currentPreset_);
+        int currentNote = notePresets_[currentPreset_][beamId];
         if(value)
         {
-            myOSCManager_->sendNote(beamIdToNoteMap[beamId], 120, 0);
+            myOSCManager_->sendNote(currentNote, 120, 0);
+            if(lcd_)
+            {
+               lcd_->setCursor(beamId*2,1);
+               lcd_->print((char)LCD_CUSTOM_NOTE);
+            }
         }
         else
         {
-            myOSCManager_->sendNote(beamIdToNoteMap[beamId], 0, 0);
+            myOSCManager_->sendNote(currentNote, 0, 0);
+            if(lcd_)
+            {
+               lcd_->setCursor(beamId*2,1);
+               lcd_->print(" ");
+            }
         }
 }
 
--- LaserKeyboard.h
+++ LaserKeyboard.h
@@ -1,9 +1,26 @@
 #include "OSCManager.h"
 #include "Adafruit_MCP23017.h"
+#include <EEPROM.h>
+#include <vector>
+#include <array>
+#include <LiquidCrystal_I2C.h>
+
 #define NB_BEAM 7
+#define BANK_SIZE 32
+#define NB_PRESET_ADDRESS 31
+
+//TODO: put definition in common
+#define LCD_CUSTOM_NOTE 0
 
 class LaserKeyboard{
   public:
+
+  std::vector<std::array<int, NB_BEAM>> notePresets_;
+
+  
+  int currentPreset_ = 0;
+  LiquidCrystal_I2C * lcd_;
+  
   int beamIdToNoteMap[NB_BEAM];
   int beamToPinMap[NB_BEAM];
   int beamStatusMap[NB_BEAM];
@@ -13,10 +30,16 @@ class LaserKeyboard{
   Adafruit_MCP23017 * mcp_;
 
   public:
-  LaserKeyboard(Adafruit_MCP23017 * mcp);
+  LaserKeyboard(Adafruit_MCP23017 * mcp, LiquidCrystal_I2C * lcdDisplay);
   void setup();
+  void loadPreset(int presetIndex);
+  void storePreset(int index, std::array<int, NB_BEAM> * thisPreset);
+  void readAllPresetsFromROM();
+  void addPresetFromCurrent();
+  int getCurrentPreset();
   void loop();
-  void setBeamIdToNote(int beamId, int note);
+  //void setBeamIdToNote(int beamId, int note);
   void process_beam_event(int beamId, bool value);
+  void displayBeamStatus(int beamId);
   
 };
